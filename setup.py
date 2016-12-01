71372805a32ce84bcd7f2cc63817c43374d26d5f
Ulrich Feindt <ulrich.feindt@fysik.su.se>
2016-11-30 17:52:43 -0800

    corrected typo

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ MAINTAINER = 'Mickael Rigault'
 MAINTAINER_EMAIL = 'mrigault@physik.hu-berlin.de'
 URL = 'https://github.com/MickaelRigault/simsurvey/'
 LICENSE = 'BSD (3-clause)'
-DOWNLOAD_URL = 'https://github.com/MickaelRigault/simsurvey/tarball/0.1.0',
+DOWNLOAD_URL = 'https://github.com/MickaelRigault/simsurvey/tarball/0.1.0'
 VERSION = '0.1.0'
 
 try:
