060e969f427466dd5b5aff69b59909e5fd5681b5
Ulrich Feindt <9607945+ufeindt@users.noreply.github.com>
2018-11-05 13:25:08 +0100

    Bump version number, change main github user

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ DISTNAME = 'simsurvey'
 AUTHOR = 'Simsurvey Developers'
 MAINTAINER = 'Ulrich Feindt' 
 MAINTAINER_EMAIL = 'ulrich.feindt@fysik.su.se'
-URL = 'https://github.com/MickaelRigault/simsurvey/'
+URL = 'https://github.com/ufeindt/simsurvey/'
 LICENSE = 'BSD (3-clause)'
-DOWNLOAD_URL = 'https://github.com/MickaelRigault/simsurvey/tarball/0.4.0'
-VERSION = '0.4.0'
+DOWNLOAD_URL = 'https://github.com/ufeindt/simsurvey/tarball/0.4.1'
+VERSION = '0.4.1'
 
 # try:
 #     from setuptools import setup, find_packages
