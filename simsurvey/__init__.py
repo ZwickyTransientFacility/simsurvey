f84c58e9d2c577cdecc1785e3af1a7afc12c85ca
Ulrich Feindt <9607945+ufeindt@users.noreply.github.com>
2018-11-05 13:23:57 +0100

    Bump version nuumber

--- simsurvey/__init__.py
+++ simsurvey/__init__.py
@@ -1,7 +1,7 @@
 
 """ Module base on astrobject, sncosmo and astropy to prepare a future survey. """
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 from .simultarget import *
 from .simulsurvey import *
