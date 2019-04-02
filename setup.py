1254237f9764d53b5a664731da6bb6f93604fd0b
Ulrich Feindt <ulrich.feindt@fysik.su.se>
2019-04-02 15:51:44 +0200

    bumped version number

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ MAINTAINER = 'Ulrich Feindt'
 MAINTAINER_EMAIL = 'ulrich.feindt@fysik.su.se'
 URL = 'https://github.com/ufeindt/simsurvey/'
 LICENSE = 'BSD (3-clause)'
-DOWNLOAD_URL = 'https://github.com/ufeindt/simsurvey/tarball/0.4.5'
-VERSION = '0.4.5'
+DOWNLOAD_URL = 'https://github.com/ufeindt/simsurvey/tarball/0.5.0'
+VERSION = '0.5.0'
 
 try:
     from setuptools import setup, find_packages
